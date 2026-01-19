3841959d9d5cb9eb98c695ad489fc382d90e6687
Henrik Uggla <henrik.uggla@kristianstad.se>
2026-01-19 15:04:31 +0100

    Create urlzoomtolayer.js

---
+++ js/urlzoomtolayer.js
@@ -0,0 +1,33 @@
+function urlzoomtolayer() {
+	const zoomToLayer = getUrlParam('zoomToLayer');
+	if (zoomToLayer != null)
+	{
+		let layer;
+		if (!(layer=origo.api().getLayer(zoomToLayer)))
+		{
+			console.error('zoomToLayer ' + zoomToLayer + ' does not exist!');
+		}
+		else
+		{
+			function zoomLoop(zoomMaxAttempts, zoomAttempt = 1 ) {
+				setTimeout(() => {
+					if (zoomAttempt <= zoomMaxAttempts) {
+						const extent = layer.getSource().getExtent();
+						if (extent && Array.isArray(extent) && extent.length === 4 && extent.every(coord => Number.isFinite(coord))) {
+							/*console.log('Valid extent:', extent);*/
+							origo.api().zoomToExtent(Origo.ol.geom.Polygon.fromExtent(extent));
+						} else {
+							zoomLoop(zoomMaxAttempts, zoomAttempt + 1);
+						}
+					} else {
+						console.error('zoomToLayer: Extent not available after max attempts');
+					}
+				}, 100);
+			}
+
+			origo.api().getMap().getView().setZoom(0);
+			layer.setVisible(true);	
+			zoomLoop(100);
+		}
+	}
+}
